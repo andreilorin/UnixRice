968447ca691707df4516c4bd1e0525183f4ccb15
Andrei <andreilorin@gmail.com>
2018-12-26 15:05:18 +0000

    add check for curl. execute some commands as the logged user instead of root

--- Configurations/desktop.sh
+++ Configurations/desktop.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-set -x
-apt update && apt -y upgrade
+#
+#   Andrei Lorin's custom Ubuntu desktop setup
+#   andreilorin.com
+#   github.com/andreilorin
+#   twitter.com/andreilorin
+#
+
+# Variables
+USERNAME=`logname`
+
+# Check for curl
+echo "Looking for curl..."
+if [[ -z $(which curl) ]]; then
+	echo "Curl not found."
+	echo ""
+	echo "======================================================================================================"
+	echo " Please install curl on your system using your favourite package manager."
+	echo ""
+	echo " Restart after installing curl."
+	echo "======================================================================================================"
+	echo ""
+	exit 0
+fi
 
 echo "Start installing custom desktop software"
 
@@ -18,16 +39,16 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 ### Text editor
 apt install -y neovim
-curl -sLf https://spacevim.org/install.sh | bash    #vim .SpaceVim.d/init.toml    java layer, nerdtreee option
+su -c "curl -sLf https://spacevim.org/install.sh | bash" $USERNAME    #vim .SpaceVim.d/init.toml    java layer, nerdtreee option
 
 apt install fonts-powerline    #then change in gnome-tweaks the font to roboto for powerline
 
 ### Other software
-apt install -y vlc neofetch entr ddgr googler
-apt install rofi # needs shortcut created (rofi -show window)
+apt install -y vlc neofetch entr ddgr googler rofi
 
 ### Setup development environments
 #JAVA
+su - $USERNAME
 curl -s "https://get.sdkman.io" | bash
 sdk install java 11.0.1-open
 sdk install java 1.0.0-rc-10-grl
@@ -35,12 +56,13 @@ sdk install maven
 sdk install micronaut
 sdk install springboot
 
+
 #RUST
-curl https://sh.rustup.rs -sSf | sh
+su -c "curl https://sh.rustup.rs -sSf | sh" $USERNAME ############################################### need to pass 1 to this retest
 
 #GO
 curl -O https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
-tar -xvf go1.11.2.linux-amd64.tar.gz
+tar -xvf go1.11.4.linux-amd64.tar.gz
 mv go /usr/local
 mkdir ~/go_projects
 
@@ -52,7 +74,7 @@ EOF
 rm go1.11.4.linux-amd64.tar.gz
 
 #DOCKER
-apt install docker.io
+su -c "apt install -y docker.io" root
 docker pull oracle/graalvm-ce:1.0.0-rc10
 
 
