8529a4deb58e0fc62cfc4a26574286e007ac41ce
Andrei <andreilorin@gmail.com>
2019-09-11 20:35:59 +0100

    software update

--- Configurations/desktop.sh
+++ Configurations/desktop.sh
@@ -32,10 +32,7 @@ apt install -y chrome-gnome-shell #(yes chrome, even if it's for firefox)
 ### Tools required for installing other software
 #apt install -y curl
 apt install -y git
-
-### Shell
-apt install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apt install build-essential
 
 ### Text editor
 apt install -y neovim
@@ -44,27 +41,36 @@ su -c "curl -sLf https://spacevim.org/install.sh | bash" $USERNAME    #vim .Spac
 apt install fonts-powerline    #then change in gnome-tweaks the font to roboto for powerline
 
 ### Other software
-apt install -y vlc neofetch entr ddgr googler rofi
+apt install -y vlc neofetch entr ddgr googler rofi youtube-dl
+apt install rofi
+xbindkeys
 
 ### Setup development environments
 #JAVA
-su - $USERNAME
-curl -s "https://get.sdkman.io" | bash
-sdk install java 11.0.1-open
-sdk install java 1.0.0-rc-10-grl
-sdk install maven
-sdk install micronaut
-sdk install springboot
+su -c "curl -s https://get.sdkman.io | bash" $USERNAME
+#sdk install java 11.0.1-open
+#sdk install java 1.0.0-rc-10-grl
+#sdk install maven
+#sdk install micronaut
+#sdk install springboot
 
 
 #RUST
-su -c "curl https://sh.rustup.rs -sSf | sh" $USERNAME ############################################### need to pass 1 to this retest
+su -c "curl https://sh.rustup.rs -sSf | bash" $USERNAME ############################################### need to pass 1 to this retest
 
 #GO
 curl -O https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
 tar -xvf go1.11.4.linux-amd64.tar.gz
 mv go /usr/local
 mkdir ~/go_projects
+
+#REDIS
+wget
+tar
+cd
+make
+
+cd
 
 cat << EOF >> ~/.profile
 export GOPATH=$HOME/go_projects
@@ -77,7 +83,10 @@ rm go1.11.4.linux-amd64.tar.gz
 su -c "apt install -y docker.io" root
 docker pull oracle/graalvm-ce:1.0.0-rc10
 
+### Shell
+apt install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-source ~/.profile
+#source ~/.profile
 
-echo "Enjoy your new system!"
+#echo "Enjoy your new system!"
